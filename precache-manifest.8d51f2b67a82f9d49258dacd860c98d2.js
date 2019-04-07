a65f2a2ada19236539c574e578813119721717fd
garzuzo <garzuxo@gmail.com>
2019-04-07 14:15:24 -0500

    Updates

---
+++ precache-manifest.8d51f2b67a82f9d49258dacd860c98d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5adbe364817a9b3b0f",
+    "url": "/ColArt/static/js/runtime~main.da8e1bcb.js"
+  },
+  {
+    "revision": "6e4dfef67a629c77709e",
+    "url": "/ColArt/static/js/main.28a85176.chunk.js"
+  },
+  {
+    "revision": "8052404d9d91ec428516",
+    "url": "/ColArt/static/js/2.8b1cc048.chunk.js"
+  },
+  {
+    "revision": "6e4dfef67a629c77709e",
+    "url": "/ColArt/static/css/main.98a92fe5.chunk.css"
+  },
+  {
+    "revision": "8052404d9d91ec428516",
+    "url": "/ColArt/static/css/2.b85a71b0.chunk.css"
+  },
+  {
+    "revision": "3352f773a7b9cf1e951950041bf0d302",
+    "url": "/ColArt/index.html"
+  }
+];
