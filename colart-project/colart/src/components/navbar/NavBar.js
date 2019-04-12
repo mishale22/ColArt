5adbc797fb7c37d781b824fa6ad200577721d1f5
mishale22 <sami_na11@hotmail.com>
2019-04-12 10:22:24 -0500

    navbar

--- colart-project/colart/src/components/navbar/NavBar.js
+++ colart-project/colart/src/components/navbar/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends Component {
     return (
       <div className="NavBar">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#">ColArt</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -17,20 +17,7 @@ class NavBar extends Component {
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a className="nav-link" href="#">Soy Artista</a>
       </li>
     </ul>
   </div>
--- colart-project/colart/src/components/userform/UserForm.js
+++ colart-project/colart/src/components/userform/UserForm.js
@@ -5,19 +5,15 @@ class UserForm extends Component {
       <div className="UserForm w-50 float-right">
   <form>
   <div className="form-group">
-    <label forHtml="exampleInputEmail1">Email address</label>
+    <label forHtml="exampleInputEmail1">Correo Electronico</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+    <small id="emailHelp" className="form-text text-muted">Nunca compartiremos tu correo con nadie más.</small>
   </div>
   <div className="form-group">
-    <label forHtml="exampleInputPassword1">Password</label>
+    <label forHtml="exampleInputPassword1">Contraseña</label>
     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" forHtml="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
+  <button type="submit" className="btn btn-primary">Entrar</button>
 </form>
       </div>
     );
